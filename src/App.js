8d2085c08d065d6ff1949247e0e2738d1728566e
Bikash Adhikari <bikashadhikari@Bikashs-MacBook-Air.local>
2023-05-29 12:28:47 +0530

    files can be dropped anywhere on screen implemented

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import { useCallback, useState } from 'react'
+import { ToastContainer, toast } from 'react-toastify';
 import './App.scss';
 import FileUpload from './FileUpload/FileUpload';
 import FileList from './FileList/FileList';
-import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
@@ -25,18 +25,12 @@ function App() {
   }
 
   return (
-
     <div className="App">
-      <div className="title">Upload file</div>
       <FileUpload files={files} setFiles={setFiles}
         removeFile={removeFile} onDrop={onDrop} accept={"image/*,audio/*,video/*,.pdf"} />
       <FileList files={files} removeFile={removeFile} />
       <ToastContainer />
     </div>
-
-
-
-
   );
 }
 
--- src/FileUpload/FileUpload.js
+++ src/FileUpload/FileUpload.js
@@ -1,60 +1,44 @@
 import React from 'react'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faPlus } from '@fortawesome/free-solid-svg-icons'
-import './FileUpload.scss'
-import axios from 'axios'
 import { useDropzone } from "react-dropzone"
+import axios from 'axios'
+import './FileUpload.scss'
 
 const FileUpload = ({ files, setFiles, removeFile, onDrop, accept }) => {
     const { getRootProps, getInputProps, isDragActive } =
-    useDropzone({
-        accept,
-        onDrop,
-    });
-const uploadHandler = (event) => {
-    const file = event.target.files[0];
-    if(!file) return;
-    file.isUploading = true;
-    setFiles([...files, file])
-
-    // upload file
-    const formData = new FormData();
-    formData.append(
-        "newFile",
-        file,
-        file.name
-    )
-    console.log(formData, "adbbd")
-    axios.post('http://localhost:8080/upload', formData)
-        .then((res) => {
-            file.isUploading = false;
-            setFiles([...files, file])
-        })
-        .catch((err) => {
-            // inform the user
-            console.error(err)
-            removeFile(file.name)
+        useDropzone({
+            accept,
+            onDrop,
         });
-}
+    const uploadHandler = (event) => {
+        const file = event.target.files[0];
+        if (!file) return;
+        file.isUploading = true;
+        setFiles([...files, file])
+
+        // upload file
+        const formData = new FormData();
+        formData.append(
+            "newFile",
+            file,
+            file.name
+        )
+        console.log(formData, "adbbd")
+        axios.post('http://localhost:8080/upload', formData)
+            .then((res) => {
+                file.isUploading = false;
+                setFiles([...files, file])
+            })
+            .catch((err) => {
+                // inform the user
+                console.error(err)
+                removeFile(file.name)
+            });
+    }
 
     return (
         <>
             <div className="file-card">
-
-                <div className="file-inputs">
-                    <input type="file" onChange={uploadHandler} />
-                    <button>
-                        <i>
-                            <FontAwesomeIcon icon={faPlus} />
-                        </i>
-                        Upload
-                    </button>
-                </div>
-
-
-
                 <div   {...getRootProps({ className: "file-card" })}>
-
                     <div className="file-inputs">
                         <input type="file" onChange={uploadHandler} {...getInputProps()} directory="" webkitdirectory="" />
                         {isDragActive ? (
@@ -63,14 +47,12 @@ const uploadHandler = (event) => {
                             </p>
                         ) : (
                             <p className="main" >
-                                Drag’ n’ drop some files here, or click to select files
+                                Drag’ n’ drop files anywhere on the screen
                             </p>
                         )}
-
                     </div>
                     <p className="main">Suppprted</p>
                     <p className="info">Image, Audio, Video, File, Directory</p>
-
                 </div>
             </div>
         </>
